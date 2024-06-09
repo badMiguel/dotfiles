b88160dd5c80103e6d5107144227d0f23ddff3d9
Miguel Badayos <badayosbiboy@gmail.com>
2024-06-09 21:20:54 +0930

    added  vim be good plugin

--- .config/nvim/lua/nvimBTW/packer.lua
+++ .config/nvim/lua/nvimBTW/packer.lua
@@ -4,17 +4,17 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-	-- Packer can manage itself
-	use 'wbthomason/packer.nvim'
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
 
-	-- Telescope
-	use {
-		'nvim-telescope/telescope.nvim', tag = '0.1.6',
-		-- or                            , branch = '0.1.x',
-		requires = { {'nvim-lua/plenary.nvim'} }
-	}
+    -- Telescope
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.6',
+        -- or                            , branch = '0.1.x',
+        requires = { {'nvim-lua/plenary.nvim'} }
+    }
 
-	------ THEMES COLLECTION ------
+    ------ THEMES COLLECTION ------
     -- note: select colorscheme in after/colors.lua 
 
     -- rosepine theme
@@ -46,58 +46,58 @@ return require('packer').startup(function(use)
 
     -- catpuccin theme
     use ({
-		"catppuccin/nvim",
-		as = "catppuccin",
+        "catppuccin/nvim",
+        as = "catppuccin",
 
-		config = function()
-			vim.cmd('colorscheme catppuccin')
-		end
-	})
-	------ END THEMES COLLECTION ------
+        config = function()
+            vim.cmd('colorscheme catppuccin')
+        end
+    })
+    ------ END THEMES COLLECTION ------
 
-	-- Treesitter
-	use ('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
-	use ('nvim-treesitter/playground')
+    -- Treesitter
+    use ('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+    use ('nvim-treesitter/playground')
 
-	-- Harpoon
-	use ('theprimeagen/harpoon')
+    -- Harpoon
+    use ('theprimeagen/harpoon')
 
-	-- Undo Tree
-	use ('mbbill/undotree')
+    -- Undo Tree
+    use ('mbbill/undotree')
 
-	-- for git
-	use ('tpope/vim-fugitive')
+    -- for git
+    use ('tpope/vim-fugitive')
 
-	-- commenting
-	use {
-		'numToStr/Comment.nvim',
-		config = function()
-			require('Comment').setup()
-		end
-	}
+    -- commenting
+    use {
+        'numToStr/Comment.nvim',
+        config = function()
+            require('Comment').setup()
+        end
+    }
 
-	-- LSP
-	use {
-		"williamboman/mason.nvim",
-		"williamboman/mason-lspconfig.nvim",
-		"neovim/nvim-lspconfig",
-	}
+    -- LSP
+    use {
+        "williamboman/mason.nvim",
+        "williamboman/mason-lspconfig.nvim",
+        "neovim/nvim-lspconfig",
+    }
 
-	-- LSP completion
-	use {
-		'hrsh7th/cmp-nvim-lsp',
-		'hrsh7th/cmp-buffer',
-		'hrsh7th/cmp-path',
-		'hrsh7th/cmp-cmdline',
-		'hrsh7th/nvim-cmp',
-	}
+    -- LSP completion
+    use {
+        'hrsh7th/cmp-nvim-lsp',
+        'hrsh7th/cmp-buffer',
+        'hrsh7th/cmp-path',
+        'hrsh7th/cmp-cmdline',
+        'hrsh7th/nvim-cmp',
+    }
 
-	-- snippets 
-	use {
-		'L3MON4D3/LuaSnip',
-		'saadparwaiz1/cmp_luasnip',
-		'rafamadriz/friendly-snippets'
-	}
+    -- snippets 
+    use {
+        'L3MON4D3/LuaSnip',
+        'saadparwaiz1/cmp_luasnip',
+        'rafamadriz/friendly-snippets'
+    }
 
     -- use { 'christoomey/vim-tmux-navigator', }
 
@@ -105,7 +105,9 @@ return require('packer').startup(function(use)
     --     "MaximilianLloyd/ascii.nvim",
     --     requires = {
     --         "MunifTanjim/nui.nvim"
-    --     }
+    --     },
     -- })
+
+    use 'ThePrimeagen/vim-be-good'
 
 end)
