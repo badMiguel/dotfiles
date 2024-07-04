9412e7d98c1b81275459f9db9fae33a96a5c663d
miguel <badayosbiboy@gmail.com>
2024-07-04 18:33:37 +0930

    added cava on spotify workspace

--- bin/.local/scripts/workspace-startup-arrangement.sh
+++ bin/.local/scripts/workspace-startup-arrangement.sh
@@ -5,8 +5,10 @@ sleep 1
 i3-msg '[class="Google-chrome" title="primary - Google Chrome"] move to workspace 1'
 i3-msg '[class="Alacritty" title="secondary"] move to workspace 10'
 
+
 sleep 2
 
 i3-msg '[class="Alacritty" title="secondary"] move container to down'
 i3-msg '[class="Google-chrome" title="primary - Google Chrome"] move container to left; move container to left'
-
+i3-msg '[class="Alacritty" title="primary"] resize set width 1600px'
+i3-msg '[class="Alacritty" title="cava"] resize set width 700px'
