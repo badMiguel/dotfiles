d47cd931129f49a813142b570549e77a17571be2
miguel <badayosbiboy@gmail.com>
2024-07-12 13:11:43 +0930

    improved nvim netrw

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,12 @@
 require("nvimBTW")
 
 vim.g.netrw_banner = 0
+vim.g.netrw_liststyle= 3
+
+-- relative line numbers in netrw
+vim.api.nvim_exec([[
+  augroup netrw_settings
+    autocmd!
+    autocmd FileType netrw setlocal relativenumber
+  augroup END
+]], false)
