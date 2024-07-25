486e4d29c1b8ad90eba7c8e24a3d455fd1ea7ff3
miguel <badayosbiboy@gmail.com>
2024-07-25 16:52:53 +0930

    accidentally updated packer which changed lualine

--- nvim/after/plugin/lualine.lua
+++ nvim/after/plugin/lualine.lua
@@ -1,8 +1,10 @@
-require('lualine').setup{
-    options = {
-        theme = 'rose-pine',
-        icons_enabled = true,
-    }
+local rose_pine = require'lualine.themes.rose-pine'
+
+rose_pine.normal.c.bg = '#00000000'
+rose_pine.insert.c.bg = '#00000000'
+rose_pine.visual.c.bg = '#00000000'
+rose_pine.command.c.bg = '#00000000'
+
+require('lualine').setup {
+  options = { theme  = rose_pine },
 }
-
-
