1cd87434da46d119c9a14652d0014d3cef853f96
MiguelBad <badayosbiboy@gmail.com>
2024-10-06 23:17:35 +0930

    update fugitive remap

--- nvim/after/plugin/fugitive.lua
+++ nvim/after/plugin/fugitive.lua
@@ -1,4 +1,4 @@
 vim.keymap.set('n', '<leader>gs', vim.cmd.Git);
 
-vim.keymap.set('n', 'gf', '<cmd>diffget //2<CR>')
-vim.keymap.set('n', 'gj', '<cmd>diffget //3<CR>')
+vim.keymap.set('n', '<leader>gf', '<cmd>diffget //2<CR>')
+vim.keymap.set('n', '<leader>gj', '<cmd>diffget //3<CR>')
