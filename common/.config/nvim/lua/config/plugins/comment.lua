19b444ea4fcc8c0e12b77da686445be29a0b8d6d
miguel <badayosbiboy@gmail.com>
2025-03-03 17:00:59 +0930

    moved comment kemap location

--- common/.config/nvim/lua/config/plugins/comment.lua
+++ common/.config/nvim/lua/config/plugins/comment.lua
@@ -1,15 +1,24 @@
 return {
     'numToStr/Comment.nvim',
+    opts = {
+        toggler = {
+            line = 'gcc',
+            block = 'gbc',
+        },
+        opleader = {
+            line = 'gc',
+            block = 'gb',
+        }
+    },
     config = function()
-        require('Comment').setup({
-            toggler = {
-                line = 'gcc',
-                block = 'gbc',
-            },
-            opleader = {
-                line = 'gc',
-                block = 'gb',
-            }
-        })
+        local keymap = vim.api.nvim_set_keymap
+        -- comment remap
+        keymap('n', '<C-_>', '<cmd>lua require("Comment.api").toggle.linewise.current()<CR>',
+            { noremap = true, silent = true })
+        keymap('i', '<C-_>', '<Esc><cmd>lua require("Comment.api").toggle.linewise.current()<CR>gi',
+            { noremap = true, silent = true })
+        keymap('v', '<C-_>', '<esc><cmd>lua require("Comment.api").toggle.linewise(vim.fn.visualmode())<CR>',
+            { noremap = true, silent = true })
     end
+
 }
