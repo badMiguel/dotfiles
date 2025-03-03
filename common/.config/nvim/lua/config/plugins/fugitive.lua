3bb3acf7fe8531bfb7326c5ef586a52719e0358c
miguel <badayosbiboy@gmail.com>
2025-03-03 17:57:20 +0930

    fugitive config update

--- common/.config/nvim/lua/config/plugins/fugitive.lua
+++ common/.config/nvim/lua/config/plugins/fugitive.lua
@@ -1,8 +1,8 @@
 return {
-    'tpope/vim-fugitive', 
-    config = function()
-        vim.keymap.set('n', '<leader>gs', vim.cmd.Git);
-        vim.keymap.set('n', '<leader>gf', '<cmd>diffget //2<CR>')
-        vim.keymap.set('n', '<leader>gj', '<cmd>diffget //3<CR>')
-    end
+    'tpope/vim-fugitive',
+    keys = {
+        { "<leader>gs", "<cmd>Git<CR>" },
+        { "<leader>gf", "<cmd>diffget //2<CR>" },
+        { "<leader>gj", "<cmd>diffget //3<CR>" },
+    }
 }
