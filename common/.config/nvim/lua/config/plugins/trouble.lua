090ef5f000bf268675c57afd899fc02509ce645e
miguel <badayosbiboy@gmail.com>
2025-03-03 17:10:14 +0930

    adjusted trouble.nvim config

--- common/.config/nvim/lua/config/plugins/trouble.lua
+++ common/.config/nvim/lua/config/plugins/trouble.lua
@@ -1,16 +1,12 @@
 return {
     "folke/trouble.nvim",
-
-    config = function()
-        require("trouble").setup {
-            opts = {},
-            cmd = "Trouble",
-            auto_refresh = true,
-            lsp_diagnostics = true,
-        }
-
-        vim.keymap.set("n", "<leader>tt", "<cmd>Trouble diagnostics toggle<cr>")
-        vim.keymap.set("n", "[t", "<cmd>Trouble diagnostics next<cr>")
-        vim.keymap.set("n", "]t", "<cmd>Trouble diagnostics prev<cr>")
-    end
+    opts = {},
+    cmd = "Trouble",
+    auto_refresh = true,
+    lsp_diagnostics = true,
+    keys = {
+        { "<leader>tt", "<cmd>Trouble diagnostics toggle<cr>" },
+        { "[t",         "<cmd>Trouble diagnostics next<cr>" },
+        { "]t",         "<cmd>Trouble diagnostics prev<cr>" },
+    }
 }
