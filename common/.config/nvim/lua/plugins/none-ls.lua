c9f868235c4bee40ebe9421377c81db04072f262
miguel <badayosbiboy@gmail.com>
2025-03-04 11:43:21 +0930

    none-ls config adjustment

--- common/.config/nvim/lua/plugins/none-ls.lua
+++ common/.config/nvim/lua/plugins/none-ls.lua
@@ -1,29 +1,31 @@
 return {
-    "nvimtools/none-ls.nvim",
-    dependencies = {
+    {
+        "nvimtools/none-ls.nvim",
+        opts = function(_, opts)
+            local nls = require("null-ls")
+            opts.sources = {
+                nls.builtins.formatting.prettier.with({
+                    extra_args = {
+                        "--tab-width", "4",
+                        "--print-width", "100",
+                        "--semi", "true",
+                        "--single-quote", "false",
+                        "--trailing-comma", "es5",
+                        "--bracket-spacing", "true",
+                        "--arrow-parens", "always",
+                    },
+                }),
+                nls.builtins.formatting.black,
+                nls.builtins.formatting.gofumpt,
+            }
+            -- followed syntax from:
+            -- https://github.com/LazyVim/LazyVim/blob/ec5981dfb1222c3bf246d9bcaa713d5cfa486fbd/lua/lazyvim/plugins/extras/lsp/none-ls.lua#L4
+        end
+    },
+    {
         'jay-babu/mason-null-ls.nvim',
         opts = {
             ensure_installed = { "prettier", "black" }
         }
-    },
-    opts = function(_, opts)
-        local nls = require("null-ls")
-        opts.sources = {
-            nls.builtins.formatting.prettier.with({
-                extra_args = {
-                    "--tab-width", "4",
-                    "--print-width", "100",
-                    "--semi", "true",
-                    "--single-quote", "false",
-                    "--trailing-comma", "es5",
-                    "--bracket-spacing", "true",
-                    "--arrow-parens", "always",
-                },
-            }),
-            nls.builtins.formatting.black,
-            nls.builtins.formatting.gofumpt,
-        }
-        -- followed syntax from: 
-        -- https://github.com/LazyVim/LazyVim/blob/ec5981dfb1222c3bf246d9bcaa713d5cfa486fbd/lua/lazyvim/plugins/extras/lsp/none-ls.lua#L4
-    end
+    }
 }
