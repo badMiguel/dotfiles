02acb3120f027a3cf41e8cfcf81c81f4a17a9243
miguel <badayosbiboy@gmail.com>
2025-03-04 17:25:05 +0930

    updates

--- common/.config/nvim/lua/plugins/harpoon.lua
+++ common/.config/nvim/lua/plugins/harpoon.lua
@@ -1,13 +1,15 @@
 return {
-    'theprimeagen/harpoon',
-    branch = 'harpoon2',
-    dependencies = { 'nvim-lua/plenary.nvim' },
-    keys = {
-        { "<leader>a", function() require("harpoon"):list():add() end, },
-        { "<C-e>",     function() require("harpoon").ui:toggle_quick_menu(require("harpoon"):list()) end, },
-        { "<C-h>",     function() require("harpoon"):list():select(1) end, },
-        { "<C-j>",     function() require("harpoon"):list():select(2) end, },
-        { "<C-k>",     function() require("harpoon"):list():select(3) end, },
-        { "<C-l>",     function() require("harpoon"):list():select(4) end, },
-    }
+    {
+        'theprimeagen/harpoon',
+        branch = 'harpoon2',
+        keys = {
+            { "<leader>a", function() require("harpoon"):list():add() end, },
+            { "<C-e>",     function() require("harpoon").ui:toggle_quick_menu(require("harpoon"):list()) end, },
+            { "<C-h>",     function() require("harpoon"):list():select(1) end, },
+            { "<C-j>",     function() require("harpoon"):list():select(2) end, },
+            { "<C-k>",     function() require("harpoon"):list():select(3) end, },
+            { "<C-l>",     function() require("harpoon"):list():select(4) end, },
+        }
+    },
+    { 'nvim-lua/plenary.nvim', lazy = true },
 }
