372fd5d12b21e24fad2d9b27344f034e321b0c28
MiguelBad <badayosbiboy@gmail.com>
2025-03-09 13:41:21 +0930

    update

--- common/.config/nvim/lua/plugins/none-ls.lua
+++ common/.config/nvim/lua/plugins/none-ls.lua
@@ -17,16 +17,6 @@ return {
                 }),
                 nls.builtins.formatting.black,
                 nls.builtins.formatting.gofumpt,
-                nls.builtins.formatting.phpcsfixer.with({
-                    env = { PHP_CS_FIXER_IGNORE_ENV = "1" },
-                    extra_args = {
-                        "--rules=braces_position",
-                        "--braces_position[control_structures]=same",
-                        "--braces_position[functions]=same",
-                        "--braces_position[anonymous_functions]=same",
-                        "--braces_position[classes]=same"
-                    }
-                }),
             }
             -- followed syntax from:
             -- https://github.com/LazyVim/LazyVim/blob/ec5981dfb1222c3bf246d9bcaa713d5cfa486fbd/lua/lazyvim/plugins/extras/lsp/none-ls.lua#L4
