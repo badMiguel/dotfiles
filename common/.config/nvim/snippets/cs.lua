326e7239d71b8436bd9c8f555d5809c6d998dd5c
MiguelBad <badayosbiboy@gmail.com>
2025-07-25 17:04:22 +0930

    edited controller snippet

--- common/.config/nvim/snippets/cs.lua
+++ common/.config/nvim/snippets/cs.lua
@@ -1,27 +1,58 @@
 local ls = require("luasnip")
+
 local s = ls.snippet
 local t = ls.text_node
-local i = ls.insert_node                  -- input
-local rep = require("luasnip.extras").rep -- input repeat
+local i = ls.insert_node
+local f = ls.function_node
+local rep = require("luasnip.extras").rep -- repeat input
+
+local function get_root_dir()
+    local lsp_clients = vim.lsp.get_active_clients({ bufnr = 0 })
+
+    if #lsp_clients > 0 then
+        local root_path = lsp_clients[1].config.root_dir
+        return vim.fn.fnamemodify(root_path, ":t")
+    end
+
+    -- when lsp not yet loaded
+    local root = vim.fn.finddir(".git", ".;")
+    if root == "" then
+        root = vim.fn.findfile("*.sln", ".;")
+    end
+
+    if root ~= "" then
+        local root_path = vim.fn.fnamemodify(root, ":p:h:h")
+        return vim.fn.fnamemodify(root_path, ":t")
+    end
+
+    return "Namespace"
+end
+
+local function get_file_name()
+    return vim.fn.expand("%:t:r")
+end
 
 return {
     s("controller", {
         t({
-            "using Microsoft.AspNetCore.Mvc;",
             "using System.Text.Encodings.Web;",
+            "using Microsoft.AspNetCore.Mvc;",
             "",
         }),
         t({ "", "", "" }),
-        -- todo get root dir name and file name
-        t("namespace "), i(1, "ProjName"), t(".Controllers;"),
+        f(function()
+            return string.format("namespace %s.Controller;", get_root_dir())
+        end),
         t({ "", "", "" }),
-        t("public class "), i(2, "FileName"), t(" : Controller"),
+        f(function()
+            return string.format("public class %s : Controller", get_file_name())
+        end),
         t({ "", "{", "", }),
-        t("    public string "), i(3, "Index"), t("()"),
+        t("    public IActionResult "), i(1, "Index"), t("()"),
         t({
             "",
             "    {",
-            "        return \"Hello World!\";",
+            "        return View()",
             "    }",
             "}"
         }),
