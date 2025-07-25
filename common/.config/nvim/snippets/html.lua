df11a8c9db729e5284f84b8d17b9bb1aee85d141
MiguelBad <badayosbiboy@gmail.com>
2025-07-25 17:05:10 +0930

    html snippet (for cshtml)

---
+++ common/.config/nvim/snippets/html.lua
@@ -0,0 +1,22 @@
+local ls = require("luasnip")
+
+local s = ls.snippet
+local t = ls.text_node
+local i = ls.insert_node
+local f = ls.function_node
+local rep = require("luasnip.extras").rep -- repeat input
+
+return {
+    s("cshtmlviews", {
+        t({ "@{", "" }),
+        t("     ViewDate[\""), i(1, "Title"), t("\"] = \""), i(2, "Index"), t("\";"),
+        t({
+            "",
+            "}",
+            "",
+            "<h2>Heading</h2>",
+            "",
+            "<p>This is some text</p>"
+        }),
+    }),
+}
